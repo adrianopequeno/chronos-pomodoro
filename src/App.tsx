d9f293c490ee5c104cd4b22f57774127d4d1b5a6
Adriano Pequeno <adrian.pekeno@gmail.com>
2025-04-29 23:45:55 -0300

    chore: structuring css and folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Heading } from "./components/Header";
 
-function App() {
-  const [count, setCount] = useState(0)
+import "./styles/theme.css";
+import "./styles/global.css";
+import { Timer } from "lucide-react";
 
+export const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <Heading>
+        Olá Mundo!
+        <button>
+          <Timer />
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      </Heading>
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book.
       </p>
     </>
-  )
-}
-
-export default App
+  );
+};
