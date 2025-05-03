07b0551acf6c769fd5c98e2a20e0893bd9f6a161
Adriano Pequeno <adrian.pekeno@gmail.com>
2025-05-03 13:49:45 -0300

    chore: organizing components into standardized folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import { Heading } from "./components/Header";
-
 import "./styles/theme.css";
 import "./styles/global.css";
-import { Timer } from "lucide-react";
+import { Container } from "./components/Container";
+import { Heading } from "./components/Heading";
+import { Logo } from "./components/Logo";
 
 export const App = () => {
   return (
     <>
-      <Heading>
-        Olá Mundo!
-        <button>
-          <Timer />
-        </button>
-      </Heading>
-      <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book.
-      </p>
+      <Container>
+        <Logo />
+      </Container>
+
+      <Container>
+        <Heading>MENU</Heading>
+      </Container>
     </>
   );
 };
---
+++ src/components/Container/index.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from "react";
+
+import styles from "./styles.module.css";
+
+type ContainerProps = {
+  children: ReactNode;
+};
+
+export const Container = ({ children }: ContainerProps) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <section>{children}</section>
+      </div>
+    </div>
+  );
+};
