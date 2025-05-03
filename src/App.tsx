cf74ab83d95285dfd35a1708d88417a427c797a7
Adriano Pequeno <adrian.pekeno@gmail.com>
2025-05-03 14:51:46 -0300

    feat: creating form structure

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,25 @@ export const App = () => {
       <Container>
         <CountDown />
       </Container>
+
+      <Container>
+        <form className="form" action="">
+          <div className="formRow">
+            <label htmlFor="input">task</label>
+            <input type="text" name="" id="input" />
+          </div>
+          <div className="formRow">
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className="formRow">
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0</p>
+          </div>
+          <div className="formRow">
+            <button>Enviar</button>
+          </div>
+        </form>
+      </Container>
     </>
   );
 };
