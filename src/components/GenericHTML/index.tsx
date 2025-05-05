b523f785343e21eff9b49b15a06c2e43a7ac1bd0
Adriano Pequeno <adrian.pekeno@gmail.com>
2025-05-05 13:56:21 -0300

    feat: creating GenericHtml component

---
+++ src/components/GenericHTML/index.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from "react";
+import styles from "./styles.module.css";
+
+type GenericHtmlProps = {
+  children: ReactNode;
+};
+
+export const GenericHtml = ({ children }: GenericHtmlProps) => {
+  return <div className={styles.genericHtml}>{children}</div>;
+};
