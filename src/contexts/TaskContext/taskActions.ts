e172527f63fceb548d988fc260bd841f8222a887
Adriano Pequeno <adrian.pekeno@gmail.com>
2025-05-12 22:04:04 -0300

    feat: typeScript + Reducer: creating well-typed and scalable actions

---
+++ src/contexts/TaskContext/taskActions.ts
@@ -0,0 +1,24 @@
+import { TaskModel } from "../../models/TaskModel";
+
+export enum TaskActionTypes {
+  START_TASK = "START_TASK",
+  INTERRUPT_TASK = "INTERRUPT_TASK",
+  RESET_STATE = "RESET_STATE",
+}
+
+export type TaskActionsWithPayload =
+  | {
+      type: TaskActionTypes.START_TASK;
+      payload: TaskModel;
+    }
+  | {
+      type: TaskActionTypes.INTERRUPT_TASK;
+      payload: TaskModel;
+    };
+
+export type TaskActionWithoutPayload = {
+  type: TaskActionTypes.RESET_STATE;
+};
+
+// UNION
+export type TaskActionModel = TaskActionsWithPayload | TaskActionWithoutPayload;
---
+++ src/contexts/TaskContext/taskReducer.ts
@@ -0,0 +1,21 @@
+import { TaskStateModel } from "../../models/TaskStateModel";
+import { TaskActionModel, TaskActionTypes } from "./taskActions";
+
+export const taskReducer = (
+  state: TaskStateModel,
+  action: TaskActionModel
+): TaskStateModel => {
+  switch (action.type) {
+    case TaskActionTypes.START_TASK: {
+      return state;
+    }
+    case TaskActionTypes.INTERRUPT_TASK: {
+      return state;
+    }
+    case TaskActionTypes.RESET_STATE: {
+      return state;
+    }
+  }
+  // Sempre deve retorna o estado
+  return state;
+};
