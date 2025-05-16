498a8e7fd5e10931dc3b814b3cbba359d0a60a0a
Adriano Pequeno <adrian.pekeno@gmail.com>
2025-05-15 21:52:22 -0300

    feat: Web Worker

---
+++ src/workers/timerWorker.js
@@ -0,0 +1,27 @@
+isRunning = false;
+
+self.onmessage = (event) => {
+  if (isRunning) {
+    return;
+  }
+
+  isRunning = true;
+
+  const state = event.data;
+  const { activeTask, secondsRemeining } = state;
+
+  const endDate = activeTask.startDate + secondsRemeining * 1000;
+  const now = Date.now();
+  let countDownSeconds = Math.ceil((endDate - now) / 1000);
+
+  function tick() {
+    self.postMessage(countDownSeconds);
+
+    const now = Date.now();
+    countDownSeconds = Math.floor((endDate - now) / 1000);
+
+    setTimeout(tick, 1000);
+  }
+
+  tick();
+};
