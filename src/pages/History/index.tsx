6a1063aa5ed81c7cac357ef65bba17e0069e8de7
Adriano Pequeno <adrian.pekeno@gmail.com>
2025-05-27 20:12:27 -0300

    feat: creating a dictionary to translate the task type

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -40,15 +40,23 @@ export const History = () => {
               </tr>
             </thead>
             <tbody>
-              {state.tasks.map((task) => (
-                <tr key={task.id}>
-                  <td>{task.name}</td>
-                  <td>{task.duration}min</td>
-                  <td>{formatDate(task.startDate)}</td>
-                  <td>{getTaskStatus(task, state.activeTask)}</td>
-                  <td>{task.type}</td>
-                </tr>
-              ))}
+              {state.tasks.map((task) => {
+                const taskTypeDictionary = {
+                  workTime: 'Foco',
+                  shortBreakTime: 'Descanso curto',
+                  longBreakTime: 'Descanso longo',
+                };
+
+                return (
+                  <tr key={task.id}>
+                    <td>{task.name}</td>
+                    <td>{task.duration}min</td>
+                    <td>{formatDate(task.startDate)}</td>
+                    <td>{getTaskStatus(task, state.activeTask)}</td>
+                    <td>{taskTypeDictionary[task.type]}</td>
+                  </tr>
+                );
+              })}
             </tbody>
           </table>
         </div>
